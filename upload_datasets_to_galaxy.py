2a7ed7c2d6f37c3b6965ad92b21cecc0a4abd91a
Remi Marenco <remi.marenco@gmail.com>
2016-08-05 14:44:08 -0400

    Add first verion to upload via BioBlend

--- upload_datasets_to_galaxy.py
+++ upload_datasets_to_galaxy.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import argparse
-# from bioblend.galaxy import GalaxyInstance
+from bioblend.galaxy import GalaxyInstance
 import configparser
+import os
 
 def upload_datasets_to_galaxy():
     # Arguments initialization
@@ -21,9 +22,34 @@ def upload_datasets_to_galaxy():
 
     galaxy_config = config['Galaxy']
 
-    # gi = GalaxyInstance(url=galaxy_config['url'], key=galaxy_config['api-key'])
+    gi = GalaxyInstance(url='http://127.0.0.1:8080', key='5e8cc5748922c598c1aa6ec9e605780f')
 
-    # print(gi.histories.get_histories())
+    name_folder_test = '160802_D00281L_0127_C9NPBANXX'
+    path_folder_test = './test-data/staging/' + name_folder_test
+    path_to_fastq_folder_test = os.path.join(path_folder_test, 'fastq')
+
+    # TODO: Make a loop which execute the following, for each directory found
+    libs_folder = gi.libraries.get_libraries(name=name_folder_test)
+    # TODO: Check the library does already exist
+    # Create the library with the name equal to the folder name
+    # and description 'Library' + folder_name
+    dict_library_test = gi.libraries.create_library(name_folder_test,
+                                                    description=' '.join(['Library', name_folder_test]),
+                                                    synopsis=None)
+
+    # Upload the data in the library just created
+    list_of_files = '\n'.join(os.listdir(path_to_fastq_folder_test))
+    unknow_return = gi.libraries.upload_from_galaxy_filesystem(
+            library_id=dict_library_test.get('id'),
+            filesystem_paths=list_of_files,
+            file_type='auto',
+            link_data_only='link_to_files',
+            )
+    print(unknow_return)
+    # TODO: Check if no new files, else upload them
+    # print("Already there! Skipping {0}".format(name_folder_test))
+
+    #print(gi.histories.get_histories())
 
 
 if __name__ == "__main__":
