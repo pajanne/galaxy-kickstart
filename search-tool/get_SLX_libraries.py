6ef4b2e490246b19a849afb353e678c756750091
Remi Marenco <remi.marenco@gmail.com>
2016-09-11 12:07:37 -0400

    Add a better handling of errors (still need to escalate to user => Only in logs for now

--- search-tool/get_SLX_libraries.py
+++ search-tool/get_SLX_libraries.py
@@ -10,6 +10,10 @@ def find_SLX_libraries(SLX_libraries_path='.'):
     # TODO: Think about how to make this more flexible => .loc?
 
     # TODO: Check the path
+    list_SLX_libraries_formatted = []
+    if SLX_libraries_path is None:
+        return list_SLX_libraries_formatted
+
     list_SLX_libraries = [entry
                           for entry in os.listdir(SLX_libraries_path)
                           if os.path.isdir(os.path.abspath(os.path.join(SLX_libraries_path, entry)))]
@@ -21,15 +25,18 @@ def find_SLX_libraries(SLX_libraries_path='.'):
     log.debug("Result data in find_SLX_libraries from {0}: {1}".format(SLX_libraries_path, list_SLX_libraries_formatted))
     return list_SLX_libraries_formatted
 
-def get_SLX_library_fastq_files(slx_library_path='.'):
-    log.debug("Parameter in get_SLX_library_fastq_files: {0}".format(slx_library_path))
-    slx_fastq_library_path = os.path.join(slx_library_path, 'fastq')
+def get_SLX_library_fastq_files(SLX_library_path='.'):
+    list_SLX_library_fastq_files = []
+
+    log.debug("Parameter in get_SLX_library_fastq_files: {0}".format(SLX_library_path))
+    if SLX_library_path is None:
+        return list_SLX_library_fastq_files
+
+    slx_fastq_library_path = os.path.join(SLX_library_path, 'fastq')
 
     fa_gz_list_checker = ['fq', 'gz']
     def get_last_two_extensions(string):
         return string.split('.')[-2:]
-
-    list_SLX_library_fastq_files = []
 
     for entry in os.listdir(slx_fastq_library_path):
         full_entry_path = os.path.join(slx_fastq_library_path, entry)
