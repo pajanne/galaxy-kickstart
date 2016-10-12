9f6e66827af5c373b5904f652a4b2c80dd2d9e43
pajanne <pajanne@gmail.com>
2016-10-12 17:53:52 +0100

    Tool is kind of working but dynamic_options not

--- galaxy_kickstart/get_list_of_files.py
+++ galaxy_kickstart/get_list_of_files.py
@@ -7,10 +7,10 @@ logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 log = logging.getLogger('kickstart')
 
 
-def find_sequencing_files(library_id):
+def find_sequencing_files(library_id, config_file):
     # read config from ini file
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read(config_file)
     data_config = config['Data']
     log.debug(library_id)
     sequencing_files = glob.glob("%s/*/fastq/%s*.fq.gz" % (data_config['library_root'], library_id))
