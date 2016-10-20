989798ef6b67f75a719cfbda26ea6120b768be3b
pajanne <pajanne@gmail.com>
2016-10-20 17:27:27 +0100

    Add user API Key for uploading data libraires into current history; add file types to choose from fastq or bam; improve output message; add library contents file too

--- galaxy_kickstart/kickstart.py
+++ galaxy_kickstart/kickstart.py
@@ -21,11 +21,70 @@ def get_galaxy_item(galaxy_library_contents, item_type, item_name):
             return item
 
 
+def upload_files(data_config, options, gi, user_gi, galaxy_library, file_config):
+    # user current history
+    user_current_history_id = user_gi.histories.get_current_history()['id']
+
+    # fetch contents of sequencing library in galaxy
+    galaxy_library_contents = gi.libraries.show_library(library_id=galaxy_library['id'], contents=True)
+    log.debug('>>> gi.libraries.show_library: %s' % galaxy_library_contents)
+
+    # search sequencing files on disk in library root folder
+    # import them into galaxy data libraries if they do not already exist
+    # and upload them into user's history
+    sequencing_files = glob.glob("%s/*/%s/%s*.%s" % (data_config['library_root'], file_config['subfolder'], options.library_id, file_config['extension']))
+
+    log.debug(sequencing_files)
+    tool_message = "Sequencing files *.%s found:\n" % file_config['extension']
+    if not sequencing_files:
+        tool_message += "\tNone\n"
+    for i, f in enumerate(sequencing_files, start=1):
+        # extract filename and run folder
+        path, seq_filename = os.path.split(f)
+        path, file_dir = os.path.split(path)
+        if file_dir == file_config['subfolder']:
+            path, run_folder = os.path.split(path)
+        else:
+            run_folder = file_dir
+
+        tool_message += "\t%s\t%s\n" % (i, seq_filename)
+
+        # find folder in galaxy data library if not create it
+        run_folder_in_galaxy = get_galaxy_item(galaxy_library_contents, 'folder', run_folder)
+        if not run_folder_in_galaxy:
+            # create folder in galaxy
+            run_folder_in_galaxy = gi.libraries.create_folder(library_id=galaxy_library['id'], folder_name=run_folder)[0]
+            # retrieve new library contents
+            galaxy_library_contents = gi.libraries.show_library(library_id=galaxy_library['id'], contents=True)
+
+        # find file in galaxy data library if not upload it
+        seq_file_in_galaxy = get_galaxy_item(galaxy_library_contents, 'file', seq_filename)
+        if not seq_file_in_galaxy:
+            # upload file
+            seq_file_in_galaxy = gi.libraries.upload_from_galaxy_filesystem(
+                library_id=galaxy_library['id'],
+                filesystem_paths=f,
+                folder_id=run_folder_in_galaxy['id'],
+                file_type=file_config['galaxy_type'],
+                link_data_only='link_to_files')[0]
+            tool_message += "\t\t>>> uploaded into data library %s\n" % (data_config['galaxy_library_root'])
+            # retrieve new library contents
+            galaxy_library_contents = gi.libraries.show_library(library_id=galaxy_library['id'], contents=True)
+
+        # upload file in user's current history from data library
+        user_gi.histories.upload_dataset_from_library(user_current_history_id, seq_file_in_galaxy['id'])
+        tool_message += '\t\t+++ added into current history\n'
+
+    return tool_message
+
+
 def main():
     # command line options
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", dest="config", action="store", help="The configuration file.", required=True)
+    parser.add_argument("--userapikey", dest="user_api_key", action="store", help="The current user api key.", required=True)
     parser.add_argument("--library", dest="library_id", action="store", help="The SLX id of the library to fetch.", required=True)
+    parser.add_argument("--filetypes", dest="file_types", action="store", help="The type of files to upload.", required=True)
     parser.add_argument("--output", dest="output", action="store", help="Output log file.", required=True)
     options = parser.parse_args()
 
@@ -35,11 +94,11 @@ def main():
     galaxy_config = config['Galaxy']
     data_config = config['Data']
 
-    # create bioblend galaxy instance
+    # create bioblend galaxy instance for admin
     gi = GalaxyInstance(url=galaxy_config['url'], key=galaxy_config['api-key'])
 
-    # user current history
-    user_current_history_id = gi.histories.get_current_history()['id']
+    # create bioblend galaxy instance for current user
+    user_gi = GalaxyInstance(url=galaxy_config['url'], key=options.user_api_key)
 
     # fetch sequencing library in galaxy, create it if it does not exist
     galaxy_libraries = gi.libraries.get_libraries(name=data_config['galaxy_library_root'])
@@ -51,71 +110,25 @@ def main():
         sequencing_galaxy_library = active_galaxy_libraries[0]
     log.debug(sequencing_galaxy_library)
 
-    # fetch contents of sequencing library in galaxy
-    sequencing_galaxy_library_contents = gi.libraries.show_library(library_id=sequencing_galaxy_library['id'], contents=True)
-    log.debug('>>> gi.libraries.show_library: %s' % sequencing_galaxy_library_contents)
+    log.debug(">>> File types: %s" % options.file_types)
 
-    # search sequencing files on disk in library root folder
-    # import them into galaxy data libraries if they do not already exist
-    # and upload them into user's history
-    sequencing_files = glob.glob("%s/*/fastq/%s*.fq.gz" % (data_config['library_root'], options.library_id))
-    log.debug(sequencing_files)
-    tool_message = ''
-    for i, f in enumerate(sequencing_files, start=1):
-        # extract fastq filename and run folder
-        path, fastq_filename = os.path.split(f)
-        path, fastq_dir = os.path.split(path)
-        if fastq_dir == 'fastq':
-            path, run_folder = os.path.split(path)
-        else:
-            run_folder = fastq_dir
-        log.debug(run_folder)
-        log.debug(fastq_filename)
+    tool_message  = "--------------------------------------------------------------------------------\n"
+    tool_message += "--- Upload Sequencing files ----------------------------------------------------\n"
+    tool_message += "--------------------------------------------------------------------------------\n"
+    tool_message += "Library root: %s\n" % data_config['library_root']
+    tool_message += "User API key: %s\n" % options.user_api_key
+    tool_message += "Library ID: %s\n" % options.library_id
+    tool_message += "--------------------------------------------------------------------------------\n"
+    tool_message += upload_files(data_config, options, gi, user_gi, sequencing_galaxy_library, config['contents'])
+    if 'fastq' in options.file_types:
+        tool_message += upload_files(data_config, options, gi, user_gi, sequencing_galaxy_library, config['fastq'])
+    if 'bam' in options.file_types:
+        tool_message += upload_files(data_config, options, gi, user_gi, sequencing_galaxy_library, config['bam'])
+    tool_message += "--------------------------------------------------------------------------------\n"
 
-        # find folder in galaxy data library if not create it
-        run_folder_in_galaxy = get_galaxy_item(sequencing_galaxy_library_contents, 'folder', run_folder)
-        if not run_folder_in_galaxy:
-            # create folder in galaxy
-            run_folder_in_galaxy = gi.libraries.create_folder(library_id=sequencing_galaxy_library['id'], folder_name=run_folder)[0]
-            # retrieve new library contents
-            sequencing_galaxy_library_contents = gi.libraries.show_library(library_id=sequencing_galaxy_library['id'], contents=True)
-            log.debug('>>> gi.libraries.show_library: %s' % sequencing_galaxy_library_contents)
-        log.debug('>>> run_folder_in_galaxy: %s' % run_folder_in_galaxy)
-
-        # find file in galaxy data library if not upload it
-        fastq_file_in_galaxy = get_galaxy_item(sequencing_galaxy_library_contents, 'file', fastq_filename)
-        if not fastq_file_in_galaxy:
-            # upload file
-            fastq_file_in_galaxy = gi.libraries.upload_from_galaxy_filesystem(
-                library_id=sequencing_galaxy_library['id'],
-                filesystem_paths=f,
-                folder_id=run_folder_in_galaxy['id'],
-                file_type='fastqcompressed',
-                link_data_only='link_to_files')[0]
-            tool_message += '\t%s\t%s uploaded in data library %s\n' % (i, fastq_filename, data_config['galaxy_library_root'])
-            # retrieve new library contents
-            sequencing_galaxy_library_contents = gi.libraries.show_library(library_id=sequencing_galaxy_library['id'], contents=True)
-            log.debug('>>> gi.libraries.show_library: %s' % sequencing_galaxy_library_contents)
-        log.debug('>>> fastq_file_in_galaxy: %s' % fastq_file_in_galaxy)
-
-        # upload file in user's current history from data library
-        gi.histories.upload_dataset_from_library(user_current_history_id, fastq_file_in_galaxy['id'])
-        tool_message += '\t%s\t%s added into history\n' % (i, fastq_filename)
-
-    # write output result file
+    # write output message into file
     with open(options.output, 'w') as out:
-        out.write("--------------------------------------------------------------------------------\n")
-        out.write("--- Upload FASTQ files ---------------------------------------------------------\n")
-        out.write("--------------------------------------------------------------------------------\n")
-        out.write("Library root: %s\n" % data_config['library_root'])
-        out.write("Library ID: %s\n" % options.library_id)
-        out.write("--------------------------------------------------------------------------------\n")
-        out.write("Sequencing FASTQ files found:\n")
-        for (i, f) in enumerate(sequencing_files, start=1):
-            out.write("\t%s\t%s\n" % (i, os.path.split(f)[-1]))
-        out.write("Sequencing FASTQ files uploaded:\n")
         out.write(tool_message)
-        out.write("--------------------------------------------------------------------------------\n")
 
 
 if __name__ == "__main__":
